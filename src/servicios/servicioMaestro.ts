2e796e85deb4512516689db0874ef0002de66b16
Nicolás Correa <nicolascorrea1910@gmail.com>
2025-05-20 12:06:22 -0300

    featrue: mapeo de entidades con prisma

--- src/servicios/servicioMaestro.ts
+++ src/servicios/servicioMaestro.ts
@@ -1,10 +1,10 @@
-import prisma from "src/prismaClient";  
-import { Request,Response } from "express";
+import prisma from "src/prismaClient";
+import { Request, Response } from "express";
 
 export const getMaestro = async (req: Request, res: Response) => {
   try {
-    const maestro = await prisma.maestroArticulos.findMany();
-    res.json(maestro);
+    const articulo = await prisma.articulo.findMany();
+    res.json(articulo);
   } catch (error) {
     console.error("Error fetching maestro:", error);
     res.status(500).json({ error: "Internal server error" });
