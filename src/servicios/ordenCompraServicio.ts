cf357d493eedc565ac8c2601eaec921503a08bbb
Nicolás Correa <nicolascorrea1910@gmail.com>
2025-05-21 17:50:47 -0300

    feature: modulación del servicio de Ventas

--- src/servicios/ordenCompraServicio.ts
+++ src/servicios/ordenCompraServicio.ts
@@ -2,6 +2,73 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export const crearOrdenCompra = async (data: { tamanoLote: number; montoOrden: number; proveedorId: number; ordenEstadoId: number; }) => {
-  return await prisma.ordenCompra.create({ data });
+export const generarOrdenCompra = async (articuloId: number) => {
+  const modeloLoteFijo = await prisma.modeloLoteFijo.findUnique({
+    where: { articuloId },
+  });
+
+  if (modeloLoteFijo) {
+    const articuloData = await prisma.articulo.findUnique({
+      where: { codArticulo: articuloId },
+    });
+
+    if (
+      articuloData &&
+      articuloData.stockActual >= modeloLoteFijo.puntoPedido
+    ) {
+      const ordenesExistentes = await prisma.ordenCompra.findMany({
+        where: {
+          detalles: {
+            some: { articuloId },
+          },
+          ordenEstado: {
+            nombreEstadoOrden: { in: ["Pendiente", "Enviada"] },
+          },
+        },
+      });
+
+      if (ordenesExistentes.length > 0) {
+        throw new Error(
+          `Ya existe una Orden de Compra activa (Pendiente o Enviada) para el artículo con ID ${articuloId}`
+        );
+      }
+
+      const proveedorPredeterminado = await prisma.proveedorArticulo.findFirst({
+        where: {
+          articuloId,
+          predeterminado: true,
+        },
+      });
+
+      if (!proveedorPredeterminado) {
+        throw new Error(
+          `No se encontró un proveedor predeterminado para el artículo con ID ${articuloId}`
+        );
+      }
+
+      const ultimaOrden = await prisma.ordenCompra.findFirst({
+        orderBy: { numOrdenCompra: 'desc' },
+      });
+
+      const nuevoNumOrdenCompra = (ultimaOrden?.numOrdenCompra || 0) + 1;
+
+      await prisma.ordenCompra.create({
+        data: {
+          numOrdenCompra: nuevoNumOrdenCompra,
+          tamanoLote: modeloLoteFijo.loteOptimo,
+          montoOrden: articuloData.costoCompra,
+          proveedorId: proveedorPredeterminado.proveedorId,
+          ordenEstadoId: 1,
+          detalles: {
+            create: [
+              {
+                articuloId,
+              },
+            ],
+          },
+        },
+      });
+    }
+  }
 };
+
--- src/servicios/ventaServicio.ts
+++ src/servicios/ventaServicio.ts
@@ -1,98 +1,32 @@
 import { PrismaClient } from '@prisma/client';
+import { generarOrdenCompra } from './ordenCompraServicio';
 
 const prisma = new PrismaClient();
 
 export const crearVenta = async (data: { fechaVenta: Date; montoTotalVenta: number; articulos: { articuloId: number; cantidad: number; }[]; }) => {
   return await prisma.$transaction(async (prisma) => {
-    // Crear la venta
+
     const venta = await prisma.venta.create({
       data: {
-        nroVenta: Date.now(), // Generar un número único para nroVenta
+        nroVenta: Date.now(),
         fechaVenta: data.fechaVenta,
         montoTotalVenta: data.montoTotalVenta,
       },
     });
 
-    // Crear los ArticuloVenta relacionados
-    for (const articulo of data.articulos) {
+    const crearArticuloVenta = async (nroVenta: number, articulo: { articuloId: number; cantidad: number; }) => {
       await prisma.articuloVenta.create({
         data: {
-          VentaId: venta.nroVenta, // Usar nroVenta como referencia
+          VentaId: nroVenta,
           ArticuloId: articulo.articuloId,
-          cantidadArticulo: articulo.cantidad, // Corregir el nombre del campo
+          cantidadArticulo: articulo.cantidad,
         },
       });
+    };
 
-      // Verificar el modelo de inventario del artículo
-      const modeloLoteFijo = await prisma.modeloLoteFijo.findUnique({
-        where: { articuloId: articulo.articuloId },
-      });
-
-      if (modeloLoteFijo) {
-        // Verificar si el stock actual es mayor o igual al Punto de Pedido (PP)
-        const articuloData = await prisma.articulo.findUnique({
-          where: { codArticulo: articulo.articuloId },
-        });
-
-        if (
-          articuloData &&
-          articuloData.stockActual >= modeloLoteFijo.puntoPedido
-        ) {
-          // Verificar si no hay órdenes pendientes o enviadas para el artículo
-          const ordenesExistentes = await prisma.ordenCompra.findMany({
-            where: {
-              detalles: {
-                some: { articuloId: articulo.articuloId },
-              },
-              ordenEstado: {
-                nombreEstadoOrden: { in: ["Pendiente", "Enviada"] },
-              }
-
-            }
-          });
-
-          if (ordenesExistentes.length > 0) {
-            throw new Error(`Ya existe una Orden de Compra activa (Pendiente o Enviada) para el artículo con ID ${articulo.articuloId}`);
-          }
-
-          // Generar una orden de compra en estado pendiente
-          // Buscar el proveedor predeterminado para el artículo
-          const proveedorPredeterminado = await prisma.proveedorArticulo.findFirst({
-            where: {
-              articuloId: articulo.articuloId,
-              predeterminado: true,
-            },
-          });
-
-          if (!proveedorPredeterminado) {
-            throw new Error(`No se encontró un proveedor predeterminado para el artículo con ID ${articulo.articuloId}`);
-          }
-
-          // Obtener el último numOrdenCompra de la base de datos
-          const ultimaOrden = await prisma.ordenCompra.findFirst({
-            orderBy: { numOrdenCompra: 'desc' },
-          });
-
-          const nuevoNumOrdenCompra = (ultimaOrden?.numOrdenCompra || 0) + 1;
-
-          await prisma.ordenCompra.create({
-            data: {
-              numOrdenCompra: nuevoNumOrdenCompra,
-              tamanoLote: modeloLoteFijo.loteOptimo,
-              montoOrden: articuloData.costoCompra, // Calcular el monto según el precio del artículo
-              proveedorId: proveedorPredeterminado.proveedorId, // Usar el proveedor predeterminado
-              ordenEstadoId: 1, // Estado "Pendiente"
-              detalles: {
-                create: [
-                  {
-                    articuloId: articulo.articuloId,
-                  },
-                ],
-              },
-            },
-          });
-        }
-      }
+    for (const articulo of data.articulos) {
+      await crearArticuloVenta(venta.nroVenta, articulo);
+      await generarOrdenCompra(articulo.articuloId);
     }
 
     return venta;
