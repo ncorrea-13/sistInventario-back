3be57b6d5396fb236725d572954194d40b940e28
Nicolás Correa <nicolascorrea1910@gmail.com>
2025-05-24 11:59:40 -0300

    refactor: eliminado dist

--- dist/dto/proveedorDto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toProveedorDTO = void 0;
-// Función para transformar un proveedor en un DTO
-const toProveedorDTO = (proveedor) => ({
-    id: proveedor.codProveedor,
-    nombre: proveedor.nombreProv,
-});
-exports.toProveedorDTO = toProveedorDTO;
--- dist/prismaClient.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-// Export the Prisma client instance
-exports.default = prisma;
