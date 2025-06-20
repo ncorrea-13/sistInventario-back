ebe207a9e96ebb142bff4cb5d7454097768c9fd1
Nicolás Correa <nicolascorrea1910@gmail.com>
2025-06-20 10:44:37 -0300

    fix: revision stock antes de crear la venta

--- src/servicios/ventaServicio.ts
+++ src/servicios/ventaServicio.ts
@@ -2,74 +2,85 @@ import { prisma } from '../prismaClient';
 import { generarOrdenCompra } from './ordenCompraServicio';
 
 export const crearVenta = async (data: { montoTotalVenta: number; articulos: [{ articuloId: number; cantidad: number; }]; }) => {
-    const venta = await prisma.venta.create({
-      data: {
-        fechaVenta: new Date(),
-        montoTotalVenta: data.montoTotalVenta,
-      },
-    });
-     
-    for (const articulo of data.articulos) {
-      try {
 
-        await crearArticuloVenta(venta.nroVenta, articulo);
-        // Verificar el stock actual del artículo
-        const articuloStock = await prisma.articulo.findUnique({
-          where: { codArticulo: articulo.articuloId },
-          select: { stockActual: true },
-        });
+  for (const articulo of data.articulos) {
+    try {
+      // Verificar el stock actual del artículo
+      const articuloStock = await prisma.articulo.findUnique({
+        where: { codArticulo: articulo.articuloId },
+        select: { stockActual: true },
+      });
 
-        if (!articuloStock) {
-          console.warn(`No se encontró información de stock para el artículo con ID ${articulo.articuloId}`);
-          continue;
-        }
-
-        // Actualizar el stock del artículo
-        await prisma.articulo.update({
-          where: { codArticulo: articulo.articuloId },
-          data: {
-            stockActual: {
-              decrement: articulo.cantidad,
-            },
-          },
-        });
-
-        // Verificar el stock actual del artículo
-        const modelo = await prisma.articulo.findUnique({
-          where: { codArticulo: articulo.articuloId },
-          select: { modeloInventario: true }
-        });
-        if (!modelo) {
-          continue;
-        }
-        if (modelo.modeloInventario === "LoteFijo") {
-          const modeloLoteFijo = await prisma.modeloLoteFijo.findFirst({
-            where: { articuloId: articulo.articuloId },
-          });
-
-          if (!modeloLoteFijo) {
-            console.warn(`No se encontró un modelo de lote fijo para el artículo con ID ${articulo.articuloId}`);
-            continue;
-          }
-
-          if (articuloStock.stockActual <= modeloLoteFijo.puntoPedido) {
-            // Generar una orden de compra en estado pendiente
-            await generarOrdenCompra(articulo.articuloId, modeloLoteFijo.puntoPedido);
-          }
-        }
+      if (!articuloStock) {
+        console.warn(`No se encontró información de stock para el artículo con ID ${articulo.articuloId}`);
+        continue;
       }
-      catch (error) {
-        console.error(`Error procesando el artículo con ID ${articulo.articuloId}:`, error);
-        throw error;
+
+      if (articulo.cantidad > articuloStock.stockActual) {
+        throw new Error(`No hay suficiente stock para el artículo con ID ${articulo.articuloId}. Stock actual: ${articuloStock.stockActual}, cantidad solicitada: ${articulo.cantidad}`);
+      }
+    } catch (error) {
+      console.error(`Error procesando el artículo con ID ${articulo.articuloId}:`, error);
+      throw error;
+    }
+  }
+
+  const venta = await prisma.venta.create({
+    data: {
+      fechaVenta: new Date(),
+      montoTotalVenta: data.montoTotalVenta,
+    },
+  });
+
+  for (const articulo of data.articulos) {
+    try {
+      // Actualizar el stock del artículo
+      const articuloStock = await prisma.articulo.update({
+        where: { codArticulo: articulo.articuloId },
+        data: {
+          stockActual: {
+            decrement: articulo.cantidad,
+          },
+        },
+      });
+
+      await crearArticuloVenta(venta.nroVenta, articulo);
+      // Verificar el stock actual del artículo
+      const modelo = await prisma.articulo.findUnique({
+        where: { codArticulo: articulo.articuloId },
+        select: { modeloInventario: true }
+      });
+      if (!modelo) {
+        continue;
+      }
+      if (modelo.modeloInventario === "loteFijo") {
+        const modeloLoteFijo = await prisma.modeloLoteFijo.findFirst({
+          where: { articuloId: articulo.articuloId },
+        });
+
+        if (!modeloLoteFijo) {
+          console.warn(`No se encontró un modelo de lote fijo para el artículo con ID ${articulo.articuloId}`);
+          continue;
+        }
+
+        if (articuloStock.stockActual <= modeloLoteFijo.puntoPedido) {
+          // Generar una orden de compra en estado pendiente
+          await generarOrdenCompra(articulo.articuloId, modeloLoteFijo.puntoPedido);
+        }
       }
     }
-    return venta;
+    catch (error) {
+      console.error(`Error procesando el artículo con ID ${articulo.articuloId}:`, error);
+      throw error;
+    }
+  }
+  return venta;
 
-  };
+};
 
 
 const crearArticuloVenta = async (nroVenta: number, articulo: { articuloId: number; cantidad: number; }) => {
- await prisma.articuloVenta.create({
+  await prisma.articuloVenta.create({
     data: {
       VentaId: nroVenta,
       ArticuloId: articulo.articuloId,
